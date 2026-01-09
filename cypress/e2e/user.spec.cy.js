e650f6e5ab990a58a07cae21faede0686cd582f2
Patrícia Silva <patricia.diosilva@gmail.com>
2026-01-09 16:20:54 -0300

    added Chance for information

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,6 +4,9 @@ import DashboardPage from '../pages/dashboardPage.js'
 import MyInfoPage from '../pages/MyInfoPage.js'
 import MenuPage from '../pages/menuPage.js'
 
+const Chance = require('chance');
+
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const myInfoPage = new MyInfoPage()
@@ -20,8 +23,8 @@ describe('Orange HRM Tests', () => {
 
     menuPage.accessMyInfo()
 
-    myInfoPage.fillPersonalDetails("Primeiro", "Meio", "Ultimo")
-    myInfoPage.fillEmployerDetails("123456", "123456", "123456", "2026-06-02")
+    myInfoPage.fillPersonalDetails(chance.first(), chance.word(), chance.last())
+    myInfoPage.fillEmployerDetails(chance.phone({ formatted: false }), chance.ssn(), chance.ssn(), chance.date({string: true}))
     myInfoPage.fillStatus()
     myInfoPage.saveForm()
   })
